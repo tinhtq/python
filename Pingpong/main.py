b22471e15805d5ec77580e9f77f4d58bc93eab40
tinhtq <truongquangtinh1997@gmail.com>
2024-10-15 11:15:51 +0700

    update kubernetes

--- Pingpong/main.py
+++ Pingpong/main.py
@@ -3,14 +3,17 @@ import uvicorn
 
 app = FastAPI(swagger_ui_parameters={"syntaxHighlight": False})
 
+
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
 
 @app.get("/")
 def default():
     return {"message": "default"}
 
+
 if __name__ == "__main__":
     # Start Uvicorn server when running this file
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
